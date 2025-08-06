8dc008445ed16efff9f178d35269c44dd09c3054
Wellerl <167458005+Wellerl@users.noreply.github.com>
2025-08-06 11:54:55 -0300

    Add files via upload

---
+++ scripts/deck.js
@@ -0,0 +1,182 @@
+const deck = [
+  {
+    name: "Articuno",
+    image: "../supertrunfoclashroyaleeeeee/assets/articunonormal.png",
+    attributes: {
+      strength: 80,
+      defense: 30,
+      magic: 40,
+    },
+  },
+  {
+    name: "Rayquaza",
+    image: "../supertrunfoclashroyaleeeeee/assets/rayquazanormalzao.png",
+    attributes: {
+      strength: 70,
+      defense: 50,
+      magic: 20,
+    },
+  },
+  {
+    name: "Gardevoir",
+    image: "../supertrunfoclashroyaleeeeee/assets/gardevoirnormal.png",
+    attributes: {
+      strength: 40,
+      defense: 100,
+      magic: 10,
+    },
+  },
+  {
+    name: "Giratina",
+    image: "../supertrunfoclashroyaleeeeee/assets/giratina.png",
+    attributes: {
+      strength: 60,
+      defense: 40,
+      magic: 20,
+    },
+  },
+  {
+    name: "Arceus",
+    image: "../supertrunfoclashroyaleeeeee/assets/arceus.png",
+    attributes: {
+      strength: 60,
+      defense: 40,
+      magic: 20,
+    },
+  },
+  {
+    name: "Venusaur",
+    image: "../supertrunfoclashroyaleeeeee/assets/venusaurnormal.png",
+    attributes: {
+      strength: 20,
+      defense: 30,
+      magic: 100,
+    },
+  },
+  {
+    name: "Metagross",
+    image: "../supertrunfoclashroyaleeeeee/assets/metagrossnormal.png",
+    attributes: {
+      strength: 30,
+      defense: 30,
+      magic: 90,
+    },
+  },
+  {
+    name: "Lycanrocdusk",
+    image: "../supertrunfoclashroyaleeeeee/assets/lycanrocdusknormal.png",
+    attributes: {
+      strength: 70,
+      defense: 60,
+      magic: 10,
+    },
+  },
+  {
+    name: "Lucario",
+    image: "../supertrunfoclashroyaleeeeee/assets/lucarionormal.png",
+    attributes: {
+      strength: 50,
+      defense: 80,
+      magic: 20,
+    },
+  },
+  {
+    name: "Blastoise",
+    image: "../supertrunfoclashroyaleeeeee/assets/blastoisenormal.png",
+    attributes: {
+      strength: 60,
+      defense: 40,
+      magic: 60,
+    },
+  },
+  {
+    name: "Gengar",
+    image: "../supertrunfoclashroyaleeeeee/assets/gengarnormal.png",
+    attributes: {
+      strength: 30,
+      defense: 20,
+      magic: 30,
+    },
+  },
+  {
+    name: "Volcarona",
+    image: "../supertrunfoclashroyaleeeeee/assets/volcaronanormal.png",
+    attributes: {
+      strength: 60,
+      defense: 50,
+      magic: 30,
+    },
+  },
+  {
+    name: "Poipole",
+    image: "../supertrunfoclashroyaleeeeee/assets/poipolenormal.png",
+    attributes: {
+      strength: 70,
+      defense: 30,
+      magic: 40,
+    },
+  },
+  {
+    name: "Tyranitar",
+    image: "../supertrunfoclashroyaleeeeee/assets/tyranitarnormal.png",
+    attributes: {
+      strength: 80,
+      defense: 40,
+      magic: 50,
+    },
+  },
+  {
+    name: "Charizard",
+    image: "../supertrunfoclashroyaleeeeee/assets/charizardnormal.png",
+    attributes: {
+      strength: 60,
+      defense: 70,
+      magic: 50,
+    },
+  },
+  {
+    name: "Mewtwo",
+    image: "../supertrunfoclashroyaleeeeee/assets/mewtwonormal.png",
+    attributes: {
+      strength: 60,
+      defense: 70,
+      magic: 40,
+    },
+  },
+  {
+    name: "Pikachu",
+    image: "../supertrunfoclashroyaleeeeee/assets/pikachunormalzao.png",
+    attributes: {
+      strength: 60,
+      defense: 70,
+      magic: 40,
+    },
+  },
+  {
+    name: "Groundon",
+    image: "../supertrunfoclashroyaleeeeee/assets/groundonnormal.png",
+    attributes: {
+      strength: 100,
+      defense: 80,
+      magic: 70,
+    },
+  },
+  {
+    name: "Dialga",
+    image: "../supertrunfoclashroyaleeeeee/assets/dialga.png",
+    attributes: {
+      strength: 20,
+      defense: 100,
+      magic: 40,
+    },
+  },
+  {
+    name: "Palkia",
+    image: "../supertrunfoclashroyaleeeeee/assets/palkia.png",
+    attributes: {
+      strength: 50,
+      defense: 60,
+      magic: 50,
+    },
+  },
+];
